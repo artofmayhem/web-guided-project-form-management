1202b792909e556dea2e1a86d295042891b3af37
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-15 11:07:25 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 // 👉 App contains a more sophisticated form we'll flesh out later
 import App from "./components/App";
