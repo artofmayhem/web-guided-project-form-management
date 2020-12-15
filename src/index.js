3fc0b419789385d02e626d764539a424df6b60f1
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-15 11:21:57 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ function SimpleForm() {
   const [formValues, setFormValues] = useState(initialValues);
 
   const change = (evt) => {};
+  const submit = (evt) => {};
 
   return (
     <div className="container">
