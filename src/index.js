01e68657e0551549cbc77addcb20520133a86973
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-15 11:16:20 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -17,6 +17,8 @@ function SimpleForm() {
   const [pets, setPets] = useState(petsList);
   const [formValues, setFormValues] = useState(initialValues);
 
+  const change = (evt) => {};
+
   return (
     <div className="container">
       <h1>Simple Form</h1>
